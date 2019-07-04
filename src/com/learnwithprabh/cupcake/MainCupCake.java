0191d3f31db62f093f806e616ed0901a3bcdf019
Prabhat <prabhat.yadav@in.g4s.com>
2019-07-05 02:40:49 +0530

    cupCake solution

--- src/com/learnwithprabh/cupcake/MainCupCake.java
+++ src/com/learnwithprabh/cupcake/MainCupCake.java
@@ -3,8 +3,22 @@ package com.learnwithprabh.cupcake;
 public class MainCupCake {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		totalCupCakeCount(10,2,2); //ans =9
+	}
 
+	public static int totalCupCakeCount(int budget_n, int cupcake_cost_c, int free_wrap_cont_n) {
+		int unused_wrap = 0;
+		int count_of_wrap = budget_n / cupcake_cost_c;
+		int result = count_of_wrap;
+
+		while (count_of_wrap >= free_wrap_cont_n) {
+			result = result + (count_of_wrap + unused_wrap) / free_wrap_cont_n;
+			unused_wrap = (count_of_wrap + unused_wrap) % free_wrap_cont_n;
+			count_of_wrap = (count_of_wrap + unused_wrap) / free_wrap_cont_n;
+		}
+		
+		System.out.println(result);
+		return result;
 	}
 
 }
