38ff1ebf95563e484d5e312199919387ed313e3b
Prabhat <prabhat.yadav@in.g4s.com>
2019-07-05 01:40:15 +0530

    BitCount Code

---
+++ src/com/learnwithprabh/countingBits/MainBitCount.java
@@ -0,0 +1,10 @@
+package com.learnwithprabh.countingBits;
+
+public class MainBitCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/learnwithprabh/cupcake/MainCupCake.java
@@ -0,0 +1,10 @@
+package com.learnwithprabh.cupcake;
+
+public class MainCupCake {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
