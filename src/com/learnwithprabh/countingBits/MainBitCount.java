6c861ebde40db74896133794dc33b08cb7402590
Prabhat <prabhat.yadav@in.g4s.com>
2019-07-05 03:22:01 +0530

    Binary Count

--- src/com/learnwithprabh/countingBits/MainBitCount.java
+++ src/com/learnwithprabh/countingBits/MainBitCount.java
@@ -1,32 +1,29 @@
 package com.learnwithprabh.countingBits;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MainBitCount {
 	// ned to count the index of 1 in bit stream of input.
 	public static void main(String[] args) {
-		int input = 8;
-		int bitarray[] = convertIntBit(input);
-		printIntSolution(bitarray);
-	}
-
-	public static int[] convertIntBit(int num) {
-		int bitInput[] = new int[100000];
-		int i = 0;
-		while (num!= 0) {
-			num = num/2;
-			bitInput[i] = num%2;
-			//System.out.println(bitInput[i]);
-			i++;
+		Scanner scn = new Scanner(System.in);
+		int no = scn.nextInt();
+		char[] toBin = Integer.toBinaryString(no).toCharArray();
+		int[] position = new int[toBin.length];
+		System.out.println(toBin);
+		int j = 0;
+		int length = 0;
+		for (int i = 0; i < toBin.length; i++) {
+			int pos = i;
+			if (toBin[i] == '1') {
+				position[length] = pos + 1;
+				length++;
+			}
 		}
-		return Arrays.copyOfRange(bitInput,0,i);
-	}
-
-	public static void printIntSolution(int input[]) {
-		for (int i = 0; i < input.length; i++) {
-			System.out.print(input[i]);
+		position = Arrays.copyOf(position, length);
+		for (int i : position) {
+			System.out.println(i);
 		}
-		System.out.println();
 	}
 
 }
